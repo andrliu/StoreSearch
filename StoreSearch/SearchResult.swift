17ea9ca82a1f84a58c134689d2f64b9cc6a98569
andrliu <dieseliu321@gmail.com>
2015-01-05 23:09:45 -0800

    Added a search bar and table view. The search puts fake results in the table for now.

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,15 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Andrew Liu on 1/5/15.
+//  Copyright (c) 2015 Andrew Liu. All rights reserved.
+//
+
+import Foundation
+
+class SearchResult
+{
+    var name = ""
+    var artistName = ""
+}
--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -12,7 +12,8 @@ class SearchViewController: UIViewController {
 
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
-    var searchResults = [String]()
+    var searchResults = [SearchResult]()
+    var hasSearched = false
     
     override func viewDidLoad()
     {
@@ -27,22 +28,61 @@ extension SearchViewController: UISearchBarDelegate
 {
     func searchBarSearchButtonClicked(searchBar: UISearchBar)
     {
-        searchResults = [String]()
-        for i in 0...2
+        searchBar.resignFirstResponder()
+        searchResults = [SearchResult]()
+        if searchBar.text != "justin bieber"
         {
-            searchResults.append(String(format: "Fake Result %d for '%@'", i, searchBar.text))
+            for i in 0...2
+            {
+                let searchResult = SearchResult()
+                searchResult.name = String(format: "Fake Result %d for", i)
+                searchResult.artistName = searchBar.text
+                searchResults.append(searchResult)
+            }
         }
+        hasSearched = true
         tableView.reloadData()
     }
     
-
+    func positionForBar(bar: UIBarPositioning) -> UIBarPosition
+    {
+        return .TopAttached
+    }
 }
 
 extension SearchViewController: UITableViewDataSource
 {
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath)
+    {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
+            
+    func tableView(tableView: UITableView, willSelectRowAtIndexPath indexPath: NSIndexPath) -> NSIndexPath?
+    {
+        if searchResults.count == 0
+        {
+            return nil
+        }
+        else
+        {
+            return indexPath
+        }
+    }
+            
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
-        return searchResults.count
+        if !hasSearched
+        {
+            return 0
+        }
+        else if searchResults.count == 0
+        {
+            return 1
+        }
+        else
+        {
+            return searchResults.count
+        }
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
@@ -51,9 +91,19 @@ extension SearchViewController: UITableViewDataSource
         var cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier) as UITableViewCell!
         if cell == nil
         {
-            cell = UITableViewCell(style: .Default, reuseIdentifier: cellIdentifier)
+            cell = UITableViewCell(style: .Subtitle, reuseIdentifier: cellIdentifier)
         }
-        cell.textLabel!.text = searchResults[indexPath.row]
+        if searchResults.count == 0
+        {
+            cell.textLabel!.text = "(Nothing found)"
+            cell.detailTextLabel!.text = ""
+        }
+        else
+        {
+            let searchResult = searchResults[indexPath.row]
+            cell.textLabel!.text = searchResult.name
+            cell.detailTextLabel!.text = searchResult.artistName
+        }
         return cell
     }
 }
